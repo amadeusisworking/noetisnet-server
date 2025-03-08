910206e6877991adc5b72c4c5f9a3269ce52f045
amadeusisworking <amadeusforwork@gmail.com>
2025-03-08 19:26:19 +0100

    Fixed port and environment variables

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const PORT = process.env.PORT || 5000;
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
@@ -65,7 +66,7 @@ io.on('connection', (socket) => {
     });
 });
 
-// Start Server
-server.listen(5000, () => {
-    console.log('Server running on port 5000');
+//server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
+
